f56332dc98c75e3d36e6165cbed6ec1c81c77539
c0m4r <github@wolfet.pl>
2024-01-12 21:21:13 +0100

    Update okrutnik.sh

--- okrutnik.sh
+++ okrutnik.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-set -e
 
 # Okrutnik: a bash script that helps you write correct Python code
 # Copyright (C) 2024 c0m4r
@@ -21,29 +20,45 @@ set -e
 # Vars
 # --------------------------------------------
 
-BASEDIR=$(dirname "$0")
-VERSION=1.0.14
+# Version
+VERSION=2.0.0
 
+# Toolset
+TOOLSET="bandit black codespell mypy pylint pyright pylama ruff safety"
+
+# Base dir
+BASEDIR=$(dirname "$0")
+
+# Okrutnik venv location
+VENV="${BASEDIR}/.okrutnik_venv"
+
+# Args
 ARG1=$1
 ARG2=$2
-PYENV=false
 
+# Colors
 ORANGE='\e[1;33m'
 GREEN='\e[1;32m'
 CYAN='\e[1;36m'
 RED='\e[1;31m'
 ENDCOLOR="\e[0m"
 
-echo -e "${ORANGE}Okrutnik v${VERSION} by c0m4r${ENDCOLOR}"
+# Defaults
+TOOLS_NUM=8
+ITERATION=1
+PASSED=0
+FAILED=0
 
 # --------------------------------------------
 # Functions
 # --------------------------------------------
 
+# Print something
 print() {
     echo -e "${ORANGE}${1}${ENDCOLOR}"
 }
 
+# Pass or fail check
 pass() {
     if [[ $? -eq 0 ]]; then
         echo -e "-> ${GREEN}pass${ENDCOLOR}"
@@ -56,71 +71,114 @@ pass() {
     ITERATION=$(( $ITERATION + 1 ))
 }
 
-install() {
+# Install tools
+okrutnik_install() {
+    okrutnik_which_python
     echo -e "${CYAN}Connecting to an intergalactic relay${ENDCOLOR} 📡"
-    ${PIP} install bandit black codespell mypy pylint pyright pylama ruff
+    $PYTHON_BIN -m venv ${VENV}
+    PATH="${VENV}/bin:${PATH}"
+    source ${VENV}/bin/activate    
+    okrutnik_which_pip
+    ${PIP} install --upgrade pip setuptools wheel
+    ${PIP} install --upgrade ${TOOLSET}
     echo -e "${CYAN}Receiving communication...${ENDCOLOR}"
     echo -e "-> ${GREEN}All your base are belong to us${ENDCOLOR} 👽"
+}
+
+okrutnik_uninstall() {
+    if [[ "${VENV}" ]] && [[ -d ${VENV} ]]; then
+        echo -n "${VENV} will be removed, "
+        read -p "are you sure? (y/N): " response \
+            && [[ $response == [yY] ]] \
+            || exit 1
+        rm -rv "${VENV}"
+    else
+        echo "Nothing to do. Already uninstalled."
+    fi
+}
+
+# Pip finder function
+okrutnik_which_pip() {
+    if [[ -e ${VENV}/bin/pip3 ]]; then
+        PIP="pip3"
+    else
+        PIP="pip"
+    fi
+}
+
+# Python finder function
+okrutnik_which_python() {
+    if command -v python3 &>/dev/null ; then
+        PYTHON_BIN="python3"
+    elif command -v python &>/dev/null ; then
+        PYTHON_BIN="python"
+    elif which python3 &>/dev/null ; then
+        PYTHON_BIN="python3"
+    elif which python &>/dev/null ; then
+        PYTHON_BIN="python"
+    else
+        echo "Python not found"
+        exit 1
+    fi
 }
 
 # --------------------------------------------
 # Validation / args handling
 # --------------------------------------------
 
-# Check venv
-if [ -e ${BASEDIR}/pyvenv.cfg ]; then
-    PIP="${BASEDIR}/bin/pip"
-    PATH="${BASEDIR}/bin:${PATH}"
-elif [ -e ${BASEDIR}/venv/pyvenv.cfg ]; then
-    PIP="${BASEDIR}/venv/bin/pip"
-    PATH="${BASEDIR}/venv/bin:${PATH}"
-elif [ -e ${BASEDIR}/.venv/pyvenv.cfg ]; then
-    PIP="${BASEDIR}/.venv/bin/pip"
-    PATH="${BASEDIR}/.venv/bin:${PATH}"
-elif [ -e ${BASEDIR}/.okrutnik_venv/pyvenv.cfg ]; then
-    PIP="${BASEDIR}/.okrutnik_venv/bin/pip"
-    PATH="${BASEDIR}/.okrutnik_venv/bin:${PATH}"
-else
-    echo "Creating venv..."
-    python -m venv .okrutnik_venv
-    PIP="${BASEDIR}/.okrutnik_venv/bin/pip"
-    PATH="${BASEDIR}/.okrutnik_venv/bin:${PATH}"
-    install
-fi
-
-set +e
+echo -e "${ORANGE}Okrutnik v${VERSION} by c0m4r${ENDCOLOR}"
 
 # Check args
-if [[ "$ARG1" == "--help" ]]; then
-    echo "Usage: ./okrutnik.sh [--ignore] TARGET"
+if [[ "$ARG1" == "-h" ]] || [[ "$ARG1" == "--help" ]]; then
+    echo "Usage: ./okrutnik.sh [options] <target>"
+    echo ""
+    echo "Before <target>:"
+    echo " -s, --stop       Exit on failed linters or errors"    
+    echo ""
+    echo "Standalone:"
+    echo " -h, --help       Print this help message"
+    echo " --update         Update installed tools"
+    echo " --uninstall      Remove installed tools"
+    exit 0
+elif [[ "$ARG1" == "--update" ]]; then
+    okrutnik_install
     exit 0
 elif [[ "$ARG1" == "--install" ]]; then
-    install
+    okrutnik_install
+    exit 0
+elif [[ "$ARG1" == "--uninstall" ]]; then
+    okrutnik_uninstall
     exit 0
 elif [[ ! "$ARG1" ]]; then
-    echo "Missing TARGET"
+    echo "Missing <target>"
     exit 1
 else
     TARGET=$*
 fi
 
-set -e
-
-TOOLS_NUM=8
-ITERATION=1
-PASSED=0
-FAILED=0
+# Check venv
+if [[ -e ${VENV}/pyvenv.cfg ]]; then
+    PATH="${VENV}/bin:${PATH}"
+else
+    okrutnik_install
+fi
 
 # --------------------------------------------
 # Linters
 # --------------------------------------------
 
-if [[ "$ARG1" == "--ignore" ]]; then
-    TARGET=$(echo $@ | sed 's/\-\-ignore\ //g;')
-    set +e
+# Print Python version
+okrutnik_which_python
+${PYTHON_BIN} -V
+
+# Switch exit on failed
+if [[ "$ARG1" == "--stop" ]]; then
+    TARGET=$(echo $@ | sed -e 's/\-\-stop\ //g;')
+    set -e
 fi
 
-echo $TARGET
+# Print targets
+echo "Target(s): ${TARGET}"
 
 # ruff 0.1.12 bug workaround
 # https://github.com/astral-sh/ruff/issues/9478
